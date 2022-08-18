3b45153205d5efba3c50c66af08f81af07cfe708
dieguitocano <dieguito.kano@gmail.com>
2022-08-18 16:29:06 -0500

    Last version

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -1,28 +1,30 @@
-import axios from 'axios'
-import React from 'react'
+import axios from "axios";
+import React from "react";
 
-const CardUser = ({ user, deleteUser, updateUserById}) => {
-
-  
-
-
-
+const CardUser = ({ user, deleteUser, updateUserById }) => {
   return (
     <>
-      <article className='card-container'>
-        <h2>{user.first_name} {user.last_name}</h2>
-        <p><span>E-MAIL</span></p>
+      <article className="card-container">
+        <h2>
+          {user.first_name} {user.last_name}
+        </h2>
+        <p>
+          <span>E-MAIL</span>
+        </p>
         <p>{user.email}</p>
-        <p><span>BIRTHDAY</span></p>
+        <p>
+          <span>BIRTHDAY</span>
+        </p>
         <p>{user.birthday}</p>
         <button onClick={() => deleteUser(user.id)}>
-         <i className="fa-solid fa-trash"></i>
+          <i className="fa-solid fa-trash"></i>
         </button>
-        <button onClick={() => updateUserById(user.id)}><i className="fa-solid fa-pen-to-square"></i></button>
+        <button onClick={() => updateUserById(user.id)}>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
       </article>
-     
     </>
-  )
-}
+  );
+};
 
-export default CardUser
+export default CardUser;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,53 +1,80 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Form = ({ createNewUser }) => {
-
-  const { handleSubmit, register, reset } = useForm()
+  const { handleSubmit, register, reset } = useForm();
 
   const defaultValuesForm = {
-    email: '',
-    password: '',
-    first_name: '',
-    last_name: '',
-    birthday: ''
-  }
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    birthday: "",
+  };
 
   const submit = data => {
-    createNewUser(data)
-    console.log(data)
-    reset(defaultValuesForm)
-  }
-
+    if (createNewUser !== undefined) {
+      createNewUser(data);
+      console.log(data);
+    } else {
+      reset(defaultValuesForm);
+    }
+    reset(defaultValuesForm);
+  };
 
   return (
-
-    <form className='form-container' onSubmit={handleSubmit(submit)}>
+    <form className="form-container" onSubmit={handleSubmit(submit)}>
       <div>
         <label htmlFor="first_name"></label>
-        <input type="text" id='first_name' placeholder='Name' {...register('first_name')} />
+        <input
+          type="text"
+          id="first_name"
+          placeholder="Name"
+          {...register("first_name")}
+        />
       </div>
       <div>
         <label htmlFor="last_name"></label>
-        <input type="text" id='last_name' placeholder='Last name' {...register('last_name')} />
+        <input
+          type="text"
+          id="last_name"
+          placeholder="Last name"
+          {...register("last_name")}
+        />
       </div>
       <div>
         <label htmlFor="email"></label>
-        <input type="text" id='email' placeholder='email' {...register('email')} />
+        <input
+          type="text"
+          id="email"
+          placeholder="email"
+          {...register("email")}
+        />
       </div>
       <div>
         <label htmlFor="password"></label>
-        <input type="text" id='password' placeholder='password' {...register('password')} />
+        <input
+          type="text"
+          id="password"
+          placeholder="password"
+          {...register("password")}
+        />
       </div>
       <div>
         <label htmlFor="birthday"></label>
-        <input type="date" id='birthday' {...register('birthday')} />
+        <input
+          type="date"
+          id="birthday"
+          {...register("birthday")}
+        />
       </div>
-      <div><button className='submit-button' onClick={() => submit()}>Submit</button></div>
+      <div>
+        <button className="submit-button" >
+          Submit
+        </button>
+      </div>
     </form>
+  );
+};
 
-  )
-}
-
-export default Form
+export default Form;
